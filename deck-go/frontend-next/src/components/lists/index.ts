1b066c46f727c9b154862c3e4e69c60727afe65f
wangym <wangym@wangymdeMacBook-Pro.local>
2026-04-28 15:10:16 +0800

    deck-go: remove tracked files under frontend-next/

--- deck-go/frontend-next/src/components/lists/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./types";
-export { useListState } from "./useListState";
-export { VirtualList } from "./VirtualList";
-export { PaginatedList } from "./PaginatedList";
-export { ListSearchBar } from "./ListSearchBar";
-export { SortableHeader } from "./SortableHeader";
-export { BatchActionBar } from "./BatchActionBar";
-export { InlineEdit } from "./InlineEdit";
